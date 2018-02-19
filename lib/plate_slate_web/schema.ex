31470966ad5361c8c616411a57b838fe2b818419
Don Pflaster <dpflaster@gmail.com>
2018-02-19 08:11:58 -0500

    Create first resolver to make first query work

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -10,7 +10,11 @@ defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
 
   query do
-    # <<Ignore this for now>>
+    field :menu_items, list_of(:menu_item) do
+      resolve fn _, _, _ ->
+        {:ok, Repo.all(Menu.Item)}
+      end
+    end
   end
 
   object :menu_item do
